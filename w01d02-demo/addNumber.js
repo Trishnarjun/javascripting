e6bfb0868afe2d0adf8372afd398e929a7c324c5
Trishnarjun Kunamalla <trishnarjun.k@gmail.com>
2022-03-08 17:23:37 +0000

    making a function

--- w01d02-demo/addNumber.js
+++ w01d02-demo/addNumber.js
@@ -1,8 +1,12 @@
-const argus = process.argv
-const newArry = argus.slice(2);
-console.log(newArry);
-let counter = 0;
-for (let i = 0; i < newArry.length; i++) {  
-  counter += Number(newArry[i]);
-  }
-console.log(counter);
+const addNumbers = function() {
+  const argus = process.argv
+  const newArry = argus.slice(2);
+  let counter = 0;
+  for (let i = 0; i < newArry.length; i++) {  
+    //adding the numbers
+    counter += Number(newArry[i]);
+    }
+  // printing result 
+  console.log(counter);
+};
+addNumbers();
